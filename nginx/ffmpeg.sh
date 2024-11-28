c5cba482f5858acd3cc0f09baa8a60f2a2e11607
M4rc311o <marcelkubin2@gmail.com>
2024-11-28 20:11:49 +0100

    ffmpeg cleanup

--- nginx/ffmpeg.sh
+++ nginx/ffmpeg.sh
@@ -1,40 +1,23 @@
-#!/bin/bash
-set -e
+#!/usr/bin/env bash
+set -Eeuo pipefail
 
 on_die ()
 {
     pkill -KILL -P $$
 }
-
-
-
-
-STREAM_NAME="$1"
-# echo "Stream Name: $STREAM_NAME" > /tmp/debug.txt 
-
-# API_RESPONSE=$(curl -s "http://flask/api/keys/$1") 
-# echo "API Response: $API_RESPONSE" > /tmp/check_user_id.txt
-
-USER_ID=$(curl -s "http://flask/api/keys/$STREAM_NAME" | jq -r '.data.user_id')
-# echo "User ID: $USER_ID" > /tmp/check_user_id2.txt 
-
 trap 'on_die' TERM
 
+STREAM_NAME="$1"
 
-
-
+USER_ID=$(curl -s "http://flask/api/keys/$STREAM_NAME" | jq -r '.data.user_id')
 
 RTMP_INPUT="rtmp://127.0.0.1:1935/live/$STREAM_NAME"
-# echo "$RTMP_INPUT" > /tmp/input.txt
 
 OUTPUT_DIR="/var/hls/tmp_hls/$USER_ID"
-# echo "$OUTPUT_DIR" > /tmp/output.txt
 SCREENSHOT_DIR="/var/hls/screenshots"
 RECORDINGS_DIR="/var/hls/recordings"
 
-
-
- /usr/bin/ffmpeg -y -i "$RTMP_INPUT" \
+/usr/bin/ffmpeg -y -i "$RTMP_INPUT" \
      -map 0:v? -vf "fps=1/10,scale=400:-1" -q:v 2 -update 1 "$SCREENSHOT_DIR/${USER_ID}.jpg" \
      -map 0:v? -map 0:a? -c:v copy -c:a copy -f mpegts "${RECORDINGS_DIR}/${USER_ID}.ts" \
     -preset fast -g 48 -sc_threshold 0 \
@@ -47,8 +30,5 @@ RECORDINGS_DIR="/var/hls/recordings"
     -master_pl_name master.m3u8 \
     -f hls -hls_time 2 -hls_list_size 300 \
     -hls_segment_filename "$OUTPUT_DIR/${USER_ID}_v%v/${USER_ID}_sequence%d.ts" \
-    "$OUTPUT_DIR/${USER_ID}_v%v/media.m3u8"   
-
-
-
+    "$OUTPUT_DIR/${USER_ID}_v%v/media.m3u8"
 wait
